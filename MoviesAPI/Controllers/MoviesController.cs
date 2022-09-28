7302617b3b426a7746f9ca2e6be519bf85c5db8a
pRob3 <bubblan@slaf.se>
2022-09-28 13:29:19 +0200

    delete movie

--- MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -159,6 +159,23 @@ namespace MoviesAPI.Controllers
             return NoContent();
         }
 
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var movie = await _context.Movies.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(movie);
+            await _context.SaveChangesAsync();
+            await _fileStorageService.DeleteFile(movie.Poster, containerName);
+            
+            return NoContent();
+        }
+
         private void AnnotateActorsOrder(Movie movie) {
             if (movie.MoviesActors != null)
             {                
