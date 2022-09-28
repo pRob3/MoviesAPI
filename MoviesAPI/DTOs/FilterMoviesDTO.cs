fe8cd8a000f0cdb70547376770fb778431e6c1d2
pRob3 <bubblan@slaf.se>
2022-09-28 15:26:25 +0200

    null fix

--- MoviesAPI/DTOs/FilterMoviesDTO.cs
+++ MoviesAPI/DTOs/FilterMoviesDTO.cs
@@ -16,7 +16,7 @@ namespace MoviesAPI.DTOs
                 return new PaginationDTO() { Page = Page, RecordsPerPage = RecordsPerPage };
             }
         }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public int GenreId { get; set; }
         public bool InTheaters { get; set; }
         public bool UpcomingReleases { get; set; }
